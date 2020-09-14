b31522f575dd7b9c7c8f9bc4b29600cbc9a35ba1
masterbento <bentolima100@gmail.com>
2020-09-14 18:58:28 -0100

    urls changes

--- shop/models.py
+++ shop/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 
 # Create your models here.
-
+#admin credentials
+# #bentolima
+#123shop456
 
 class Category(models.Model):
     name= models.CharField(max_length=200,db_index=True)
